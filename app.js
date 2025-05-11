c0e74e5fa95f1ab15d408e45a6de0d9efe23ad6c
BHUVANESHWARAN K <bhuvaneshw522@gmail.com>
2025-05-11 12:42:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/dynamic-chat-app');
+
+const app = require('express')();
+const http = require('http').Server(app);
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
